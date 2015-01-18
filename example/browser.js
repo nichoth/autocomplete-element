898ba8852a70e98107661eb6533a0acdbaea0b6f
James Halliday <mail@substack.net>
2015-01-18 03:44:22 -0800

    arrow keys to shuffle through selections

--- example/browser.js
+++ example/browser.js
@@ -5,8 +5,7 @@ var months = [
     "January", "February", "March", "April", "May", "June", "July",
     "August", "September", "October", "November", "December"
 ];
-var ch = auto(input);
-input.addEventListener('keyup', function () {
+auto(input, function (ch) {
     if (!input.value.length) return ch.suggest([]);
     var matches = months.filter(function (m) {
         return lc(m.slice(0, input.value.length)) === lc(input.value);
--- index.js
+++ index.js
@@ -2,8 +2,8 @@ var isarray = require('isarray');
 
 module.exports = Auto;
 
-function Auto (elem) {
-    if (!(this instanceof Auto)) return new Auto(elem);
+function Auto (elem, fn) {
+    if (!(this instanceof Auto)) return new Auto(elem, fn);
     var self = this;
     
     var div = document.createElement('div');
@@ -19,6 +19,7 @@ function Auto (elem) {
     this.input = elem;
     this.ahead = elem.cloneNode(true);
     this.ahead.setAttribute('placeholder', '');
+    this.options = [];
     
     css(this.ahead, {
         color: '#808080',
@@ -53,14 +54,23 @@ function Auto (elem) {
     });
     var prev;
     this.box.addEventListener('mousemove', function (ev) {
-        onmouseout();
+        unhover();
         if (ev.target === this) return;
-        ev.target.style.backgroundColor = 'blue';
-        ev.target.style.color = 'white';
-        prev = ev.target;
+        hover(ev.target);
     });
-    this.box.addEventListener('mouseout', onmouseout);
-    function onmouseout (ev) {
+    this.box.addEventListener('click', function (ev) {
+        if (ev.target === this) return;
+        self.input.value = ev.target.textContent;
+        self.suggest();
+    });
+    this.box.addEventListener('mouseout', unhover);
+    
+    function hover (elem) {
+        elem.style.backgroundColor = 'blue';
+        elem.style.color = 'white';
+        prev = elem;
+    }
+    function unhover (ev) {
         if (prev) {
             prev.style.backgroundColor = 'inherit';
             prev.style.color = 'inherit';
@@ -73,13 +83,50 @@ function Auto (elem) {
             self.input.value = self.ahead.value;
             ev.preventDefault();
         }
+        else if (ev.which === 38 || ev.keyCode === 38) { // up
+            unhover();
+            var ix = -1;
+            if (prev) ix = self.options.indexOf(prev.textContent);
+            var len = self.box.children.length;
+            prev = self.box.children[(ix - 1 + len) % len];
+            self.set(prev.textContent);
+            hover(prev);
+        }
+        else if (ev.which === 40 || ev.keyCode === 40) { // down
+            unhover();
+            var ix = -1;
+            if (prev) ix = self.options.indexOf(prev.textContent);
+            prev = self.box.children[(ix + 1) % self.box.children.length];
+            self.set(prev.textContent);
+            hover(prev);
+        }
+    });
+    var previnput;
+    this.input.addEventListener('keyup', function (ev) {
+        if (ev.which === 9 || ev.keyCode === 9
+        || ev.which === 38 || ev.keyCode === 38
+        || ev.which === 40 || ev.keyCode === 40) {
+            return;
+        }
+        if (previnput !== self.input.value) {
+            previnput = self.input.value;
+            if (fn) fn.call(self, self, ev);
+        }
     });
 }
 
+Auto.prototype.set = function (txt) {
+    this.input.value = txt;
+    this.ahead.value = txt;
+};
+
 Auto.prototype.suggest = function (sgs) {
     if (!sgs) sgs = [ '' ];
-    else if (!isarray(sgs)) sgs = [ sgs ];
-    this.ahead.value = this.input.value
+    else if (!isarray(sgs)) sgs = [ sgs ].filter(Boolean);
+    this.options = sgs;
+    
+    if (!sgs[0]) this.ahead.value = '';
+    else this.ahead.value = this.input.value
         + (sgs[0] || '').slice(this.input.value.length)
     ;
     if (sgs.length <= 1) return css(this.box, { display: 'none' });;
